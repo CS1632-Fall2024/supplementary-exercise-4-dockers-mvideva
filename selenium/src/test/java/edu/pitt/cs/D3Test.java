8c4fe57bcbe5c151300325ad3ce2110b5320da92
Magda Videva <magdavideva@gmail.com>
2024-11-23 01:43:32 -0500

    Updated ChromeDriver dependencies

--- selenium/src/test/java/edu/pitt/cs/D3Test.java
+++ selenium/src/test/java/edu/pitt/cs/D3Test.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import org.junit.runners.MethodSorters;
 import static org.hamcrest.MatcherAssert.assertThat;
